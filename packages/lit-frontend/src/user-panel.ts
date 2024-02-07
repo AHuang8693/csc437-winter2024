6f33a52c5801f8811234aa520e57f10dcef60b36
AHuang8693 <Andy.Huang8693@gmail.com>
2024-02-07 10:20:02 -0800

    rename test image, weapon/system/trait classes moved to section, token renaming

--- packages/lit-frontend/src/user-panel.ts
+++ packages/lit-frontend/src/user-panel.ts
@@ -4,29 +4,68 @@ import {ToggleSwitchElement } from "./toggle-switch.ts"
 
 @customElement("user-panel")
 class UserPanelElement extends LitElement {
+  @property()
+  avatar: string = "";
+
   render() {
     return html`
-      <img src="/assets/testSmile.png">
-    <toggle-switch @change=${this._toggleDarkMode}>
-            Dark Mode
-          </toggle-switch>`;
+    <ul>
+    <li class="header">
+      <img src=${this.avatar} />
+      <h1><slot name="name">Your Name</slot></h1>
+    </li>
+    <li>
+      <toggle-switch @change=${this._toggleDarkMode}>
+        Dark Mode
+      </toggle-switch>
+    </li>`;
   }
 
   static styles = css`
-    h1 {
-      font: var(--size-type-xlarge) var(--font-family-display);
-    }
-
-    .fancy {
-      font-family: var(--font-family-body);
-      font-style: italic;
-      color: var(--color-accent);
-    }
-
-    ::slotted(*) {
-      display: inline;
-    }
-  `;
+  * {
+  margin: 0;
+  box-sizing: border-box;
+}
+ul {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--color-background);
+  color: var(--color-text);
+  border: 1px solid var(--color-accent);
+  border-radius: var(--size-corner-medium);
+  padding: var(--size-spacing-small);
+  width: min-content;
+  box-shadow: var(--shadow-dropdown);
+}
+li {
+  white-space: nowrap;
+  border-color: var(--color-accent);
+  border-width: var(--line-weight-superfine);
+}
+li.header {
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: end;
+  line-height: var(--font-line-height-display);
+}
+li:first-child {
+  border-bottom-style: solid;
+}
+li:last-child {
+  border-top-style: solid;
+}
+img {
+  display: inline;
+  height: var(--size-icon-large);
+}
+h1 {
+  font-size: var(--size-type-mlarge);
+  line-height: var(--font-line-height-display);
+  white-space: normal;
+  text-align: right;
+}
+`;
 
   _toggleDarkMode(ev: InputEvent) {
     const target = ev.target as ToggleSwitchElement;
